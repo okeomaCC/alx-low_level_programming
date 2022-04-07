5e2128d4932fee340b8d5e70e03caca5d1d171af
okeomaCC <nwagwuokeoma@gmail.com>
2022-04-07 05:35:45 -0700

    print _putchar

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar"
+/**
+ * main - Entry point
+ * Description: prints '_putchar'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar"
+	int i = o;
+
+	while (str[i] != '\0')
+	{
+		char  c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
